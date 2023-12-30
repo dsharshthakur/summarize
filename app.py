21c2f7c08ae59b032bfca5a7f4428db608548f5d
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2023-12-31 00:09:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,10 +53,10 @@ if  chck_box1 == True and submit_btn == True:
    chck_box2 == False
    st.markdown("<h4>Response:</h4>", unsafe_allow_html = True)
    answer = generate_response(user_text)
-   st.info(answer)
+   st.succes(answer)
    st.write(f"You wrote {len(user_text)} characters.")
 
-elif chck_box2 == True and submt_btn == True:
+elif chck_box2 == True and submit_btn == True:
    lang = st.text_input(label = "Enter the language to translate in." , value = None)
 
 
@@ -64,8 +64,8 @@ if lang != None:
    try:
       st.markdown("<h4>Response:</h4>", unsafe_allow_html = True)
       answer = generate_response(user_text, lang)
-      st.info(answer)
-      st.write(lang)
+      st.success(answer)
+     
    except:
       st.warning("Cannot translate in this language.")
  
