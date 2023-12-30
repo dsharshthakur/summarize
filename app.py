8b7d666657a18187c2e43cdfee9ff04aaf7a40f2
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2023-12-30 22:45:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ model = ChatGoogleGenerativeAI(model = "gemini-pro" , google_api_key = key)
 
 def generate_response(text, language = None):
     #template
-    if st.session_state["submit"] == True and language is not  None:
+    if language is not  None:
         template = '''Translate the given text in {language}.The text is:\n{text}'''
         prompt = PromptTemplate(input_variables = ["text"], template = template)
     
