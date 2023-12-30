7b0cd9a76da28eee04ee3cd90578d3250917c3de
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2023-12-30 20:52:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains.summarize import load_summarize_chain
+from langchain.text_splitter import  RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+import streamlit as st
+
+st.title("Summarization")
+user_text = st.text_area(label = "Copy paste the text here:" ,height = 200)
+
+#key
+load_dotenv()
+project_key = os.environ["key"]
+
+#model
+model = ChatGoogleGenerativeAI(model = "gemini-pro" , google_api_key = project_key)
+
+def generate_response(text):
+    #template
+
+    template = '''You are a helpfull assistant that summarizes a large text in such a way that it can be easily understood by any user.
+    The text is :\n{text}'''
+    prompt = PromptTemplate(input_variables = ["text"], template = template)
+
+    docs = [Document(page_content = text)]
+    #chain
+    chain= load_summarize_chain(llm = model , chain_type="stuff" ,prompt = prompt, verbose = False)
+    #generate
+    response = chain.run(docs)
+    return  response
+
+st.markdown("<br>" , unsafe_allow_html = True)
+
+col1 , col2 , col3 = st.columns(3)
+with col2:
+    generatebtn = st.button(label = "Start", use_container_width=True)
+
+if generatebtn:
+    st.markdown("<h4>Response:</h4>", unsafe_allow_html = True)
+    answer = generate_response(user_text)
+    st.info(answer)
+    st.write(f"You wrote {len(user_text)} characters.")
