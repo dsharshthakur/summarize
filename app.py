961b9abbefe397018bd91827c981c3c08c32a6fb
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2023-12-30 22:19:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def generate_response(text, language = None):
         template = '''Translate the given text in {language}.The text is:\n{text}'''
         prompt = PromptTemplate(input_variables = ["text"], template = template)
     
-    else:        
+    if  st.session_state["summarize"] == True
         template = '''You are a helpfull assistant that summarizes a large text in such a way that it can be easily understood by any user.Also add appropriate main heading.
         The text is :\n{text}'''
         prompt = PromptTemplate(input_variables = ["text"], template = template)
@@ -40,7 +40,7 @@ st.markdown("<br>" , unsafe_allow_html = True)
 
 col1 , col2 , col3 ,col4= st.columns(4)
 with col2:
-    generatebtn = st.button(label = "Summarize", use_container_width=True)
+    generatebtn = st.button(label = "Summarize", use_container_width=True, key = "summarize")
 with col3:
     translatebtn = st.button(label = "Translate" , use_container_width = True, key = "translate")
 
