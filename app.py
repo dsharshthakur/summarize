b4273cb66042d65cd5bd5842fbd9ad7f02758e74
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2023-12-30 23:54:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ def generate_response(text, language = None):
     if language != None       :
         template = '''Translate the given text in {language}.The text is:\n{text}'''
         prompt = PromptTemplate(input_variables = ["text"], template = template)
-    
-    if  st.session_state["summarize"] == True:
+    else:
         template = '''You are a helpfull assistant that summarizes a large text in such a way that it can be easily understood by any user.Also add appropriate main heading.
         The text is :\n{text}'''
         prompt = PromptTemplate(input_variables = ["text"], template = template)
@@ -39,30 +38,22 @@ def generate_response(text, language = None):
 
 st.markdown("<br>" , unsafe_allow_html = True)
 
-lang = None
 
    
 col1 , col2 , col3 ,col4= st.columns(4)
 with col2:
-    generatebtn = st.button(label = "Summarize", use_container_width=True, key = "summarize")
-with col3:
-    translatebtn = st.button(label = "Translate" , use_container_width = True, key = "translate")
+    cbox = st.radio(label = "Choose an option:" , options =["Summarize" , "Translate"])
 
-if generatebtn:
-    translatebtn == False
+if  chck_box == "Summarize":
     st.markdown("<h4>Response:</h4>", unsafe_allow_html = True)
     answer = generate_response(user_text)
     st.info(answer)
     st.write(f"You wrote {len(user_text)} characters.")
 
-if translatebtn == True:
+elif chck_box == "Translate":
    lang = st.text_input(label = "Enter the language to translate in." , value = None)
 
-   
-
-st.info(lang)
 if lang != None:
-   st.write(lang)
    try:
       st.markdown("<h4>Response:</h4>", unsafe_allow_html = True)
       answer = generate_response(user_text, lang)
